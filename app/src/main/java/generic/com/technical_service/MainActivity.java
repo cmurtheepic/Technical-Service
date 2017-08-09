67d870dfa40c96b974ed4ead1db98a8dd3866043
Connor Murry <CMURTHEEPIC@gmail.com>
2017-08-09 03:57:39 -0500

    17.8.9

---
+++ app/src/main/java/generic/com/technical_service/MainActivity.java
@@ -0,0 +1,13 @@
+package generic.com.technical_service;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
